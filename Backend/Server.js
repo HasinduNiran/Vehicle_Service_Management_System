effdddd67878d16b3056da3c5297e762594f5d72
Niran B G S H <127426411+HasinduNiran@users.noreply.github.com>
2024-01-29 12:20:27 +0530

    Template created

---
+++ Backend/Server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+
+const PORT = process.env.PORT || 8076;
+app.use(cors());
+app.use(bodyParser.json());
+
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    // useCreateIndex:true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify:false
+},)
+    .then(() => console.log('connected successfully'))
+    .catch((err) => { console.error(err); });
+
+//Routers require kranna   
+
+/*
+const studentRouter = require("./routes/student.js");
+// http://localhost:8071/student
+
+app.use("/student", studentRouter);
+*/
+
+
+
+
+app.listen(PORT, () => {
+    console.log('Server is up and runnig on port number:', PORT);
+})
