67b7bbf31b75aee8b57a2200b7bc26fc10e63917
Niran B G S H <127426411+HasinduNiran@users.noreply.github.com>
2024-02-17 01:04:59 +0530

    Update Vehicle.js

--- Backend/Models/Vehicle.js
+++ Backend/Models/Vehicle.js
@@ -1,2 +1,25 @@
 
 const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const vehicleSchema = new Schema({
+
+    Register_Number: {
+        type: String,
+        required: true
+    },
+    Model: {
+        type: String,
+        required: true
+    },
+    Owner: {
+        type: String,
+        required: true
+    }
+
+})
+
+const vehicle = mongoose.model("vehicle", vehicleSchema);
+
+module.exports = vehicle;
