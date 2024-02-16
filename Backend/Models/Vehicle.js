a99a4879f5ab3b46249808be1843145d2340379d
Niran B G S H <127426411+HasinduNiran@users.noreply.github.com>
2024-02-16 16:03:08 +0530

    add

--- Backend/Models/Vehicle.js
+++ Backend/Models/Vehicle.js
@@ -1,1 +1,18 @@
+<<<<<<< Updated upstream
 //n
+=======
+import mongoose from "mongoose"
+
+const vehicleSchema = mongoose.Schema(
+
+{
+
+
+
+
+
+}
+
+);
+export const vehicle = mongoose.model('car',vehicleSchema);
+>>>>>>> Stashed changes
