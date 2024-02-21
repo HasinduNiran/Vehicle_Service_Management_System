7c88ed6ca57fee7a2c607033034732b6cde1fa12
KasuniWijesiri <152061729+KasuniWijesiri@users.noreply.github.com>
2024-02-21 16:18:40 +0530

    customer Model&Route

---
+++ Backend/Models/UserAccount.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const customerSchema = mongoose.Schema(
+    {
+        firstName:{
+            type: String,
+            required:true
+        },
+        lastName:{
+            type: String,
+            required:true
+        },
+        NIC:{
+            type: String,
+            required:true 
+        },
+        phone:{
+            type: String,
+            required:true 
+        },
+        
+        email:{
+            type: String,
+            required:true 
+        },
+       
+        Username:{
+            type: String,
+            required:true 
+        },
+        password:{
+            type: String,
+            required:true 
+        },
+        
+
+    },
+    {
+        timestamps:true,
+    },
+
+);
+
+export const Customer = mongoose.model('Customer',customerSchema);
