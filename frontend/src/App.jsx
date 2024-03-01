f9a91e89b23d6c846247978caebf1b15ee426199
KavindiPathiraja <155365777+KavindiPathiraja@users.noreply.github.com>
2024-03-02 01:11:54 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+
+
+const App = () => {
+  return (
+    <Routes>
+      
+      
+    </Routes>
+  );
+};
+
+export default App;
