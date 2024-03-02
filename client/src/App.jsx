4dbda2f54a814b08620b915b4645b0b05f7171b7
Niran B G S H <127426411+HasinduNiran@users.noreply.github.com>
2024-03-02 17:09:35 +0530

    route delete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Route, Routes,Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 
 import React from 'react'
 
