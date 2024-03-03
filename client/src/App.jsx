b3965b28e4cb17a30c1786f788224aaf6b9631ac
Niran B G S H <127426411+HasinduNiran@users.noreply.github.com>
2024-03-03 12:38:24 +0530

    Frontend basic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-import {Route, Routes} from 'react-router-dom';
-
 import React from 'react'
-
-function App() {
+import {Routes,Route} from 'react-router-dom'
+import ShowVehicle from './assets/pages/Vehicle/ShowVehicle'
+import CreateVehicle from './assets/pages/Vehicle/CreateVehicle'
+import EditVehicle from './assets/pages/Vehicle/EditVehicle'
+import ReadOneVehicle from './assets/pages/Vehicle/ReadOneVehicle'
+import DeleteVehicle from './assets/pages/Vehicle/DeleteVehicle'
+const App = () => {
   return (
-  <Routes>
-    <Route path></Route>
+    <Routes>
 
+    <Route path='/' element={<ShowVehicle/>}/>
+    <Route path='/vehicle/create' element={<CreateVehicle/>}/>
+    <Route path='/vehicle/edit/:id' element={<EditVehicle/>}/>
+    <Route path='/vehicle/get/:id' element={<ReadOneVehicle/>}/>
+    <Route path='/vehicle/delete/:id' element={<DeleteVehicle/>}/>
 
-  </Routes>
+    </Routes>  
+     
+      
   )
 }
 
