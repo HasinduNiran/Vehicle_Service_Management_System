9174cabf8ceb7b161b8a15c354d5fa8c95ead13b
Yenura <Yenura02@gmail.com>
2024-03-03 15:12:15 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-import {Route, Routes} from 'react-router-dom';
-
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import ShowAllFeedback from './pages/Feedback/ShowAllFeedback';
+import CreateFeedback from './pages/Feedback/CreateFeedback';
+import ReadOneFeedback from './pages/Feedback/ReadOneFeedback';
+import DeleteFeedback from './pages/Feedback/DeleteFeedback';
+import EditFeedback from './pages/Feedback/EditFeedback';
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';
 
 function App() {
   return (
-  <Routes>
-    <Route path></Route>
-
-
-  </Routes>
-  )
+    <Routes>
+      <Route path='/' element={<ShowAllFeedback />} />
+      <Route path='/Feedback/Create' element={<CreateFeedback />} />
+      <Route path='/Feedback/Read/:id' element={<ReadOneFeedback />} />
+      <Route path='/Feedback/Delete/:id' element={<DeleteFeedback />} />
+      <Route path='/Feedback/Edit/:id' element={<EditFeedback />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
