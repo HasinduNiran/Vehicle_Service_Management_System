0406fc638dfa43b9b809c308bffe5a568472808d
Yenura <Yenura02@gmail.com>
2024-03-04 12:24:25 +0530

    Update EditFeedback.jsx

--- client/src/assets/pages/Feedback/EditFeedback.jsx
+++ client/src/assets/pages/Feedback/EditFeedback.jsx
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import BackButton from '../../components/BackButton';
-import Spinner from '../components/Spinner';
+import Spinner from '../../components/Spinner';
 import axios from 'axios';
 import { useNavigate ,useParams } from 'react-router-dom';
 
