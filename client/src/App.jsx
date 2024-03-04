358445d895074e921caf834dcc724e53dcd4f45a
Isuru66 <145270583+Isuru66@users.noreply.github.com>
2024-03-04 19:48:29 +0530

    booking delete frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import React from 'react'
 import ShowAllBooking from './assets/pages/Booking/ShowAllBooking';
 import CreateBooking from './assets/pages/Booking/CreateBooking';
 import EditBooking from './assets/pages/Booking/EditBooking';
+import DeleteBooking from './assets/pages/Booking/DeleteBooking';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
     <Route path ='/show-all'  element={<ShowAllBooking/>}/>
     <Route path='/create' element={<CreateBooking/>}/>
     <Route path='/edit/:id' element={<EditBooking/>}/>
-
+    <Route path='booking/delete/:id' element={<DeleteBooking/>}/>
 
   </Routes>
   )
---
+++ client/src/assets/components/Spinner.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Spinner = () => {
+  // Your Spinner component code here
+  return <div className="spinner">Loading...</div>;
+};
+
+export default Spinner;
