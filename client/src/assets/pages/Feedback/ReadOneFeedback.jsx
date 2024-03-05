dda2d857a35b1d1cd11e18c6741e3141c1839ee5
Yenura <Yenura02@gmail.com>
2024-03-06 00:01:37 +0530

    formatting

--- client/src/assets/pages/Feedback/ReadOneFeedback.jsx
+++ client/src/assets/pages/Feedback/ReadOneFeedback.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import Spinner from '../../components/Spinner';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import Spinner from "../../components/Spinner";
 
 const ReadOneFeedback = () => {
   const [feedback, setFeedback] = useState({});
@@ -10,12 +10,13 @@ const ReadOneFeedback = () => {
 
   const fetchFeedback = () => {
     setLoading(true);
-    axios.get(`http://localhost:8076/api/feedback/${id}`)
+    axios
+      .get(`http://localhost:8076/api/feedback/${id}`)
       .then((response) => {
         setFeedback(response.data);
       })
       .catch((error) => {
-        console.error('Error fetching feedback:', error);
+        console.error("Error fetching feedback:", error);
         // Handle error state or display error message to the user
       })
       .finally(() => {
@@ -28,16 +29,20 @@ const ReadOneFeedback = () => {
   }, [id]);
 
   return (
-    <div className='p-4'>
-      <h1 className='text-3xl my-4'>Read One Feedback</h1>
+    <div className="p-4">
+      <h1 className="text-3xl my-4">Read One Feedback</h1>
       {loading ? (
         <Spinner />
       ) : (
-        <div className='flex flex-col border-2 border-sky-400 rounded-xl p-4'>
+        <div className="flex flex-col border-2 border-sky-400 rounded-xl p-4">
           {Object.entries(feedback).map(([key, value]) => (
-            <div className='my-4' key={key}>
-              <span className='text-xl mr-4 text-gray-500'>{key}</span>
-              <span>{key === 'date_of_service' ? new Date(value).toLocaleString() : value}</span>
+            <div className="my-4" key={key}>
+              <span className="text-xl mr-4 text-gray-500">{key}</span>
+              <span>
+                {key === "date_of_service"
+                  ? new Date(value).toLocaleString()
+                  : value}
+              </span>
             </div>
           ))}
         </div>
--- client/src/assets/pages/Feedback/ShowAllFeedback.jsx
+++ client/src/assets/pages/Feedback/ShowAllFeedback.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { AiOutlineEdit } from 'react-icons/ai';
-import { BsInfoCircle } from 'react-icons/bs';
-import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
 
 const ShowAllFeedback = () => {
   const [feedbacks, setFeedbacks] = useState([]);
@@ -14,13 +14,16 @@ const ShowAllFeedback = () => {
     setLoading(true);
     setError(null);
 
-    axios.get('/feedback')
+    axios
+      .get("/feedback")
       .then((response) => {
         setFeedbacks(response.data.data);
       })
       .catch((error) => {
-        console.error('Error fetching feedback:', error);
-        setError('An error occurred while fetching the feedback. Please try again later.');
+        console.error("Error fetching feedback:", error);
+        setError(
+          "An error occurred while fetching the feedback. Please try again later."
+        );
       })
       .finally(() => {
         setLoading(false);
@@ -28,11 +31,14 @@ const ShowAllFeedback = () => {
   }, []);
 
   return (
-    <div className='p-4'>
-      <div className='flex justify-between items-center'>
-        <h1 className='text-3xl my-8'>All Feedbacks</h1>
-        <Link to='/feedback/create' className='bg-green-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
-          <MdOutlineAddBox className='text-sky-800 text-4xl' />
+    <div className="p-4">
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl my-8">All Feedbacks</h1>
+        <Link
+          to="/feedback/create"
+          className="bg-green-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          <MdOutlineAddBox className="text-sky-800 text-4xl" />
         </Link>
       </div>
       {loading ? (
@@ -40,42 +46,66 @@ const ShowAllFeedback = () => {
       ) : error ? (
         <div>{error}</div>
       ) : (
-        <table className='w-full border-separate border-spacing-2'>
+        <table className="w-full border-separate border-spacing-2">
           <thead>
             <tr>
-              <th className='border border-green-800 rounded-md'>Name</th>
-              <th className='border border-green-800 rounded-md'>Email</th>
-              <th className='border border-green-800 rounded-md'>Phone Number</th>
-              <th className='border border-green-800 rounded-md'>Employee</th>
-              <th className='border border-green-800 rounded-md'>Date of Service</th>
-              <th className='border border-green-800 rounded-md'>Message</th>
-              <th className='border border-green-800 rounded-md'>Actions</th>
+              <th className="border border-green-800 rounded-md">Name</th>
+              <th className="border border-green-800 rounded-md">Email</th>
+              <th className="border border-green-800 rounded-md">
+                Phone Number
+              </th>
+              <th className="border border-green-800 rounded-md">Employee</th>
+              <th className="border border-green-800 rounded-md">
+                Date of Service
+              </th>
+              <th className="border border-green-800 rounded-md">Message</th>
+              <th className="border border-green-800 rounded-md">Actions</th>
             </tr>
           </thead>
           <tbody>
-            {feedbacks.map((feedback) => (
-              <tr key={feedback._id} className='h-8'>
-                <td className='border border-gray-600 rounded-md'>{feedback.name}</td>
-                <td className='border border-gray-600 rounded-md'>{feedback.email}</td>
-                <td className='border border-gray-600 rounded-md'>{feedback.phone_number}</td>
-                <td className='border border-gray-600 rounded-md'>{feedback.employee}</td>
-                <td className='border border-gray-600 rounded-md'>{feedback.date_of_service}</td>
-                <td className='border border-gray-600 rounded-md'>{feedback.message}</td>
-                <td className='border border-gray-600 rounded-md'>
-                  <div className='flex justify-center gap-x-4'>
-                    <Link to={`/feedback/get/${feedback._id}`}>
-                      <BsInfoCircle className='text-2xl text-green-800' />
-                    </Link>
-                    <Link to={`/feedback/edit/${feedback._id}`}>
-                      <AiOutlineEdit className='text-2xl text-yellow-600' />
-                    </Link>
-                    <Link to={`/feedback/delete/${feedback._id}`}>
-                      <MdOutlineDelete className='text-2xl text-red-600' />
-                    </Link>
-                  </div>
+            {feedbacks && feedbacks.length > 0 ? (
+              feedbacks.map((feedback) => (
+                <tr key={feedback._id} className="h-8">
+                  <td className="border border-gray-600 rounded-md">
+                    {feedback.name}
+                  </td>
+                  <td className="border border-gray-600 rounded-md">
+                    {feedback.email}
+                  </td>
+                  <td className="border border-gray-600 rounded-md">
+                    {feedback.phone_number}
+                  </td>
+                  <td className="border border-gray-600 rounded-md">
+                    {feedback.employee}
+                  </td>
+                  <td className="border border-gray-600 rounded-md">
+                    {feedback.date_of_service}
+                  </td>
+                  <td className="border border-gray-600 rounded-md">
+                    {feedback.message}
+                  </td>
+                  <td className="border border-gray-600 rounded-md">
+                    <div className="flex justify-center gap-x-4">
+                      <Link to={`/feedback/get/${feedback._id}`}>
+                        <BsInfoCircle className="text-2xl text-green-800" />
+                      </Link>
+                      <Link to={`/feedback/edit/${feedback._id}`}>
+                        <AiOutlineEdit className="text-2xl text-yellow-600" />
+                      </Link>
+                      <Link to={`/feedback/delete/${feedback._id}`}>
+                        <MdOutlineDelete className="text-2xl text-red-600" />
+                      </Link>
+                    </div>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="7" className="text-center">
+                  No feedbacks available
                 </td>
               </tr>
-            ))}
+            )}
           </tbody>
         </table>
       )}
