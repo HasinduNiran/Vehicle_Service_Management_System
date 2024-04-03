46b42b46db768af2f75803e7f200d6d56545f208
Niran B G S H <127426411+HasinduNiran@users.noreply.github.com>
2024-04-03 07:56:33 +0530

    update showinventory

--- client/src/assets/pages/Inventory/ShowInventory.jsx
+++ client/src/assets/pages/Inventory/ShowInventory.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useRef } from "react";
 import axios from "axios";
 import Spinner from "../../components/Spinner";
 import { Link } from "react-router-dom";
