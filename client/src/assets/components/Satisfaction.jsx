c5175473c9ad8f0e6e3c2b29290f0effde7568d6
Niran B G S H <127426411+HasinduNiran@users.noreply.github.com>
2024-04-03 07:37:30 +0530

    home update

---
+++ client/src/assets/components/Satisfaction.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './../Styles/style-starter.css'; 
+
+const Satisfaction = () => {
+    return (
+        <div className="w3l-bottom-grids">
+            <div className="container-fluid px-0">
+                <div className="bottomhny-grids-sec">
+                    <div className="bottomhny-1">
+                        <div className="bottomhny-gd-ingf">
+                            <h4>Effortlessly streamline your vehicle maintenance tasks with our user-friendly platform.</h4>
+                        </div>
+                    </div>
+                    <div className="bottomhny-1 bottomhny-2">
+                        <div className="bottomhny-gd-ingf">
+                            <h4>Revolutionize the way you manage your fleet with our cutting-edge service solution.</h4>
+                        </div>
+                    </div>
+                    <div className="bottomhny-1 bottomhny-1-img">
+                        <div className="bottomhny-gd-ingf">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Satisfaction;
