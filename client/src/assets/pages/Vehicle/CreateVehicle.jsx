3d2d2ed40d0fc89799be349ac626cdbb486a3134
Niran B G S H <127426411+HasinduNiran@users.noreply.github.com>
2024-04-04 22:21:36 +0530

    add vehicle css add

--- client/src/assets/pages/Vehicle/CreateVehicle.jsx
+++ client/src/assets/pages/Vehicle/CreateVehicle.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import './styles.css'
 
 const CreateVehicle = () => {
     const [Register_Number, setRegister_Number] = useState('');
@@ -66,26 +67,27 @@ const CreateVehicle = () => {
     };
 
     return (
-        <div>
-            <div className='flex justify-between items-center'>
-                <h1 className='text-2xl font-bold'>Create Vehicle</h1>
-            </div>
-            <form onSubmit={handleSubmit}>
+        <div >
+            <div className='form-container'>
+                
+                <h1 className='title' >Create Vehicle</h1>
+           
+            <form onSubmit={handleSubmit} className='form' >
                 <div className='mt-4'>
                     <label className='block'>Vehicle Number</label>
-                    <input type='text' className='border border-gray-600 rounded-md w-full p-2' value={Register_Number} onChange={(e) => setRegister_Number(e.target.value)} maxLength={8}/>
+                    <input type='text' className='text-box' value={Register_Number} onChange={(e) => setRegister_Number(e.target.value)} maxLength={8}/>
                 </div>
                 <div className='mt-4'>
                     <label className='block'>Make</label>
-                    <input type='text' className='border border-gray-600 rounded-md w-full p-2' value={Make} onChange={(e) => setMake(e.target.value)} />
+                    <input type='text' className='text-box' value={Make} onChange={(e) => setMake(e.target.value)} />
                 </div>
                 <div className='mt-4'>
                     <label className='block'>Model</label>
-                    <input type='text' className='border border-gray-600 rounded-md w-full p-2' value={Model} onChange={(e) => setModel(e.target.value)} />
+                    <input type='text' className='text-box' value={Model} onChange={(e) => setModel(e.target.value)} />
                 </div>
                 <div className='mt-4'>
                     <label className='block'>Select Year</label>
-                    <select className='border border-gray-600 rounded-md w-full p-2' value={selectedYear} onChange={(e) => setSelectedYear(e.target.value)}>
+                    <select className='text-box' value={selectedYear} onChange={(e) => setSelectedYear(e.target.value)}>
                         <option value=''>Select Year</option>
                         {[...Array(40)].map((_, index) => {
                             const year = new Date().getFullYear() - index;
@@ -95,11 +97,11 @@ const CreateVehicle = () => {
                 </div>
                 <div className='mt-4'>
                     <label className='block'>Engine Details</label>
-                    <input type='text' className='border border-gray-600 rounded-md w-full p-2' value={Engine_Details} onChange={(e) => setEngine_Details(e.target.value)} />
+                    <input type='text' className='text-box' value={Engine_Details} onChange={(e) => setEngine_Details(e.target.value)} />
                 </div>
                 <div className='mt-4'>
                     <label className='block'>Transmission Details</label>
-                    <select className='border border-gray-600 rounded-md w-full p-2' value={Transmission_Details} onChange={(e) => setTransmission_Details(e.target.value)}>
+                    <select className='text-box' value={Transmission_Details} onChange={(e) => setTransmission_Details(e.target.value)}>
                         <option value=''>Select Transmission</option>
                         <option value='Automatic'>Automatic</option>
                         <option value='Manual'>Manual</option>
@@ -107,26 +109,27 @@ const CreateVehicle = () => {
                 </div>
                 <div className='mt-4'>
                     <label className='block'>Vehicle Color</label>
-                    <input type='text' className='border border-gray-600 rounded-md w-full p-2' value={Vehicle_Color} onChange={(e) => setVehicle_Color(e.target.value)} />
+                    <input type='text' className='text-box' value={Vehicle_Color} onChange={(e) => setVehicle_Color(e.target.value)} />
                 </div>
                 <div className='mt-4'>
                     <label className='block'>Vehicle Features</label>
-                    <input type='text' className='border border-gray-600 rounded-md w-full p-2' value={Vehicle_Features} onChange={(e) => setVehicle_Features(e.target.value)} />
+                    <input type='text' className='text-box' value={Vehicle_Features} onChange={(e) => setVehicle_Features(e.target.value)} />
                 </div>
                 <div className='mt-4'>
                     <label className='block'>Condition Assessment</label>
-                    <input type='text' className='border border-gray-600 rounded-md w-full p-2' value={Condition_Assessment} onChange={(e) => setCondition_Assessment(e.target.value)} />
+                    <input type='text' className='text-box' value={Condition_Assessment} onChange={(e) => setCondition_Assessment(e.target.value)} />
                 </div>
                 <div className='mt-4'>
                     <label className='block'>Vehicle Owner</label>
-                    <input type='text' className='border border-gray-600 rounded-md w-full p-2' value={Owner} onChange={(e) => setOwner(e.target.value)} />
+                    <input type='text' className='text-box' value={Owner} onChange={(e) => setOwner(e.target.value)} />
                 </div>
                 <div className='mt-4'>
-                    <button type='submit' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+                    <button type='submit' className='create'>
                         {loading ? 'Creating...' : 'Create Vehicle'}
                     </button>
                 </div>
             </form>
+            </div>
         </div>
     );
 };
