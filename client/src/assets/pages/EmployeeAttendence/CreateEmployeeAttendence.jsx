e66ab71eaf91cbdd572a04930d1fb5a73fad1adf
KavindiPathiraja <155365777+KavindiPathiraja@users.noreply.github.com>
2024-04-04 22:04:22 +0530

    add working hours to attendence

--- client/src/assets/pages/EmployeeAttendence/CreateEmployeeAttendence.jsx
+++ client/src/assets/pages/EmployeeAttendence/CreateEmployeeAttendence.jsx
@@ -1,125 +1,177 @@
-import React, { useState , useEffect  } from 'react';
+import React, { useState, useEffect } from 'react';
 import BackButton from '../../components/BackButton';
 import Spinner from '../../components/Spinner';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-//import { useSnackbar } from 'notistack';
 
 const CreateEmployeeAttendence = () => {
   const [EmpID, setEmpID] = useState('');
   const [employeeName, setemployeeName] = useState('');
-  const [Date, setDate] = useState('');
+  const [selectedDate, setSelectedDate] = useState('');
   const [InTime, setInTime] = useState('');
   const [OutTime, setOutTime] = useState('');
+  const [WorkingHours, setWorkingHours] = useState('');
   const [OThours, setOThours] = useState('');
-  
   const [employees, setEmployees] = useState([]);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  //const { enqueueSnackbar } = useSnackbar();
 
   const [selectedEmployee, setSelectedEmployee] = useState({
     EmpID: '',
     employeeName: ''
   });
 
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get('http://localhost:8076/employees')
+      .then((response) => {
+        setEmployees(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  const handleEmpIDChange = (e) => {
+    const selectedEmpID = e.target.value;
+    const selectedEmp = employees.find((emp) => emp.EmpID === selectedEmpID);
+    setSelectedEmployee({
+      ...selectedEmployee,
+      EmpID: selectedEmpID,
+      employeeName: selectedEmp.employeeName,
+    });
+  };
+
+  const handleEmployeeNameChange = (e) => {
+    const selectedEmployeeName = e.target.value;
+    const selectedEmp = employees.find(
+      (emp) => emp.employeeName === selectedEmployeeName
+    );
+    setSelectedEmployee({
+      ...selectedEmployee,
+      EmpID: selectedEmp.EmpID,
+      employeeName: selectedEmployeeName,
+    });
+  };
+
+  const handleInTimeChange = (e) => {
+    setInTime(e.target.value);
+    calculateHoursWorked(e.target.value, OutTime); // Call calculateHoursWorked
+  };
+
+  const handleOutTimeChange = (e) => {
+    setOutTime(e.target.value);
+    calculateHoursWorked(InTime, e.target.value); // Call calculateHoursWorked
+  };
+
+  const calculateHoursWorked = (inTime, outTime) => {
+    const inTimeParts = inTime.split(':');
+    const outTimeParts = outTime.split(':');
+
+    const inTimeDate = new Date(
+      2000,
+      0,
+      1,
+      parseInt(inTimeParts[0]),
+      parseInt(inTimeParts[1]),
+      0
+    );
+    const outTimeDate = new Date(
+      2000,
+      0,
+      1,
+      parseInt(outTimeParts[0]),
+      parseInt(outTimeParts[1]),
+      0
+    );
+
+    if (isNaN(inTimeDate.getTime()) || isNaN(outTimeDate.getTime())) {
+      console.error('Invalid input time format');
+      return;
+    }
+
+    const timeDiff = outTimeDate - inTimeDate;
+    const hoursWorked = timeDiff / (1000 * 60 * 60);
+    const normalWorkingHours = 8;
+
+    if (hoursWorked > normalWorkingHours) {
+      const overtimeHours = hoursWorked - normalWorkingHours;
+      setOThours(overtimeHours.toFixed(2));
+      setWorkingHours(normalWorkingHours.toFixed(2));
+    } else {
+      setOThours('0.00');
+      setWorkingHours(hoursWorked.toFixed(2));
+    }
+  };
+
   const handleSaveEmployeeAttendence = () => {
     const data = {
       EmpID: selectedEmployee.EmpID,
       employeeName: selectedEmployee.employeeName,
-      Date,
+      Date: selectedDate,
       InTime,
       OutTime,
-      //OThours,
-      
+      WorkingHours,
+      OThours,
     };
     setLoading(true);
     axios
       .post('http://localhost:8076/EmployeeAttendence', data)
       .then(() => {
         setLoading(false);
-        //enqueueSnackbar('Employee Created successfully', { variant: 'success' });
         navigate('/EmployeeAttendence/allEmployeeAttendence');
       })
       .catch((error) => {
         setLoading(false);
-        // alert('An error happened. Please Chack console');
-        //enqueueSnackbar('Error', { variant: 'error' });
         console.log(error);
       });
   };
 
-        useEffect(() => {
-            setLoading(true);
-            axios
-                .get('http://localhost:8076/employees')
-                .then((response) => {
-                    setEmployees(response.data.data);
-                    setLoading(false);
-                })
-                .catch((error) => {
-                    console.log(error);
-                    setLoading(false);
-                });
-        }, []);
-
-        
-        
-          const handleEmpIDChange = (e) => {
-            const selectedEmpID = e.target.value;
-            const selectedEmp = employees.find(emp => emp.EmpID === selectedEmpID);
-            setSelectedEmployee({ ...selectedEmployee, EmpID: selectedEmpID, employeeName: selectedEmp.employeeName });
-          };
-        
-          const handleEmployeeNameChange = (e) => {
-            const selectedEmployeeName = e.target.value;
-            const selectedEmp = employees.find(emp => emp.employeeName === selectedEmployeeName);
-            setSelectedEmployee({ ...selectedEmployee, EmpID: selectedEmp.EmpID, employeeName: selectedEmployeeName });
-          };
-          
-
   return (
     <div className='p-4'>
-      <BackButton destination='/EmployeeAttendence/allEmployeeAttendence' /> 
+      <BackButton destination='/EmployeeAttendence/allEmployeeAttendence' />
       <h1 className='text-3xl my-4'>Create Employee Attendence</h1>
       {loading ? <Spinner /> : ''}
       <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
-      <div className='my-4'>
-        <label className='text-xl mr-4 text-gray-500'>EmpID</label>
-        <select
-          value={selectedEmployee.EmpID}
-          onChange={handleEmpIDChange}
-          className='border-2 border-gray-500 px-4 py-2 w-full'
-        >
-          <option value=''>Select EmpID</option>
-          {employees.map((employee) => (
-            <option key={employee._id} value={employee.EmpID}>
-              {employee.EmpID}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className='my-4'>
-        <label className='text-xl mr-4 text-gray-500'>employeeName</label>
-        <select
-          value={selectedEmployee.employeeName}
-          onChange={handleEmployeeNameChange}
-          className='border-2 border-gray-500 px-4 py-2  w-full '
-        >
-          <option value=''>Select Employee Name</option>
-          {employees.map((employee) => (
-            <option key={employee._id} value={employee.employeeName}>
-              {employee.employeeName}
-            </option>
-          ))}
-        </select>
-      </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>EmpID</label>
+          <select
+            value={selectedEmployee.EmpID}
+            onChange={handleEmpIDChange}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          >
+            <option value=''>Select EmpID</option>
+            {employees.map((employee) => (
+              <option key={employee._id} value={employee.EmpID}>
+                {employee.EmpID}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>employeeName</label>
+          <select
+            value={selectedEmployee.employeeName}
+            onChange={handleEmployeeNameChange}
+            className='border-2 border-gray-500 px-4 py-2  w-full '
+          >
+            <option value=''>Select Employee Name</option>
+            {employees.map((employee) => (
+              <option key={employee._id} value={employee.employeeName}>
+                {employee.employeeName}
+              </option>
+            ))}
+          </select>
+        </div>
         <div className='my-4'>
           <label className='text-xl mr-4 text-gray-500'>Date</label>
           <input
             type='Date'
-            value={Date}
-            onChange={(e) => setDate(e.target.value)}
+            value={selectedDate}
+            onChange={(e) => setSelectedDate(e.target.value)}
             className='border-2 border-gray-500 px-4 py-2  w-full '
           />
         </div>
@@ -128,7 +180,7 @@ const CreateEmployeeAttendence = () => {
           <input
             type='time'
             value={InTime}
-            onChange={(e) => setInTime(e.target.value)}
+            onChange={handleInTimeChange}
             className='border-2 border-gray-500 px-4 py-2  w-full '
           />
         </div>
@@ -137,27 +189,40 @@ const CreateEmployeeAttendence = () => {
           <input
             type='time'
             value={OutTime}
-            onChange={(e) => setOutTime(e.target.value)}
+            onChange={handleOutTimeChange}
             className='border-2 border-gray-500 px-4 py-2  w-full '
           />
         </div>
-        {/* <div className='my-4'>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>WorkingHours</label>
+          <input
+            type='text'
+            value={WorkingHours}
+            readOnly
+            className='border-2 border-gray-500 px-4 py-2  w-full '
+          />
+        </div>
+        <div className='my-4'>
           <label className='text-xl mr-4 text-gray-500'>OThours</label>
           <input
             type='text'
             value={OThours}
-            onChange={(e) => setOThours(e.target.value)}
+            readOnly
             className='border-2 border-gray-500 px-4 py-2  w-full '
           />
-        </div> */}
+        </div>
+
         
-        
-        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveEmployeeAttendence}>
+
+        <button
+          className='p-2 bg-sky-300 m-2'
+          onClick={handleSaveEmployeeAttendence}
+        >
           Save
         </button>
       </div>
     </div>
   );
-}
+};
 
-export default CreateEmployeeAttendence
+export default CreateEmployeeAttendence;
--- client/src/assets/pages/EmployeeAttendence/ReportEmployeeAttendence.jsx
+++ client/src/assets/pages/EmployeeAttendence/ReportEmployeeAttendence.jsx
@@ -1,22 +1,26 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect,useState} from 'react';
 import axios from 'axios';
 import BackButton from '../../components/BackButton';
 import Spinner from '../../components/Spinner';
-import { useReactToPrint } from 'react-to-print';
+import {Link} from 'react-router-dom';
+
 
 function ShowEmployeeAttendence() {
-    const [employeesAttendance, setEmployeesAttendance] = useState([]);
+
+    const [employeesAttendence, setEmployeesAttendence] = useState([]);
     const [loading, setLoading] = useState(false);
     const [searchName, setSearchName] = useState('');
     const [searchDate, setSearchDate] = useState('');
-    const componentRef = useRef();
+    
+    //const [showType, setShowType] = useState('table');
+
 
     useEffect(() => {
         setLoading(true);
         axios
             .get('http://localhost:8076/EmployeeAttendence')
             .then((response) => {
-                setEmployeesAttendance(response.data.data);
+                setEmployeesAttendence(response.data.data);
                 setLoading(false);
             })
             .catch((error) => {
@@ -25,112 +29,130 @@ function ShowEmployeeAttendence() {
             });
     }, []);
 
-    // Report generating
-    const generatePDF = useReactToPrint({
-        content: () => componentRef.current,
-        documentTitle: 'Employee Attendence List',
-        //onAfterPrint: () => alert('Data saved in PDF'),
-    });
-
     const handleSearch = () => {
         // Filter employeesAttendance based on search criteria
-        return employeesAttendance.filter((attendance) => {
+        const filteredAttendence = employeesAttendence.filter((attendance) => {
             // Check if searchName and searchDate are empty or match the attendance record
             return (searchName === '' || attendance.employeeName.toLowerCase().includes(searchName.toLowerCase())) &&
                 (searchDate === '' || attendance.Date.includes(searchDate));
         });
+        return filteredAttendence;
     };
 
-    const filteredEmployeesAttendance = handleSearch();
+    const filteredEmployeesAttendence = handleSearch();
 
-    // Helper function to calculate overtime hours
-    const calculateOvertime = (inTime, outTime) => {
-        const inTimeDate = new Date(`01/01/2000 ${inTime}`);
-        const outTimeDate = new Date(`01/01/2000 ${outTime}`);
+    // Get unique employee names
+    const employeeNames = [...new Set(employeesAttendence.map((attendance) => attendance.employeeName))];
 
-        // Calculate time difference in milliseconds
-        const timeDiff = outTimeDate - inTimeDate;
 
-        // Convert time difference to hours
-        const hoursDiff = timeDiff / (1000 * 60 * 60);
 
-        // Subtract normal working hours (8 hours) to get overtime hours
-        const overtimeHours = Math.max(hoursDiff - 8, 0); // Ensure overtime is non-negative
+  return (
 
-        return overtimeHours;
-    };
+    
+    <div className="flex">
+      
 
-    return (
-        <div className="flex">
-            <div className='flex-1 p-4'>
-            <BackButton destination='/EmployeeAttendence/allEmployeeAttendence' /> 
-                <div className='flex justify-between items-center'>
-                    <h1 className='text-3xl my-8'>Employee Attendance List</h1>
-                    
+      <div className='flex-1 p-4'>
+      <BackButton destination='/EmployeeAttendence/allEmployeeAttendence' /> 
+      <div className='flex justify-between items-center'>
+                <h1 className='text-3xl my-8'>Employee Attendence List</h1>
+                
+    
+                
+                <div className="flex justify-center items-center mt-8">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => window.location.href='/EmployeeAttendence/create'}>
+                        Add Employee Attendence
+                    </button>
+                    <div style={{ marginLeft: '10px' }}></div> {/* Space between buttons */}
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => window.location.href='/EmployeeAttendence/reportEmployeeAttendence'}>
+                        Report
+                    </button>
                 </div>
-                <div className='flex  justify-between mb-4'>
-                    <div className='flex items-center'> {/* Wrap select and input in a flex container */}
-                        <select
-                            value={searchName}
-                            onChange={(e) => setSearchName(e.target.value)}
-                            className='p-2 border border-gray-300 rounded mr-2' >
-                            <option value=''>All Employees</option>
-                            {[...new Set(employeesAttendance.map((attendance) => attendance.employeeName))].map((name, index) => (
-                                <option key={index} value={name}>
-                                    {name}
-                                </option>
-                            ))}
-                        </select>
-                        <input
-                            type='month'
-                            value={searchDate}
-                            onChange={(e) => setSearchDate(e.target.value)}
-                            className='p-2 border border-gray-300 rounded'/>
-                    </div>
-                    
-                </div>
-                {loading ? (
-                    <Spinner />
-                ) : (
-                    <>
-                        <table className='w-full border-separate border-spacing-2' ref={componentRef}>
-                            <thead>
-                                <tr>
-                                    <th className='border border-slate-600 rounded-md'>No</th>
-                                    <th className='border border-slate-600 rounded-md'>EmpID</th>
-                                    <th className='border border-slate-600 rounded-md'>employeeName</th>
-                                    <th className='border border-slate-600 rounded-md max-md:hidden'>Date</th>
-                                    <th className='border border-slate-600 rounded-md max-md:hidden'>InTime</th>
-                                    <th className='border border-slate-600 rounded-md'>OutTime</th>
-                                    <th className='border border-slate-600 rounded-md'>OThours</th>
-                                    
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {filteredEmployeesAttendance.map((EmployeeAttendance, index) => (
-                                    <tr key={EmployeeAttendance._id} className='h-8'>
-                                        <td className='border border-slate-700 rounded-md text-center'>{index + 1}</td>
-                                        <td className='border border-slate-700 rounded-md text-center'>{EmployeeAttendance.EmpID}</td>
-                                        <td className='border border-slate-700 rounded-md text-center max-md:hidden'>{EmployeeAttendance.employeeName}</td>
-                                        <td className='border border-slate-700 rounded-md text-center max-md:hidden'>{EmployeeAttendance.Date}</td>
-                                        <td className='border border-slate-700 rounded-md text-center max-md:hidden'>{EmployeeAttendance.InTime}</td>
-                                        <td className='border border-slate-700 rounded-md text-center max-md:hidden'>{EmployeeAttendance.OutTime}</td>
-                                        <td className='border border-slate-700 rounded-md text-center'>{calculateOvertime(EmployeeAttendance.InTime, EmployeeAttendance.OutTime)}</td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                        {/* report button */}
-                        <div className="flex justify-center items-center mt-8">
-                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={generatePDF}>
-                                Generate PDF
-                            </button>
-                        </div>
-                    </>
-                )}
+            </div>
+
+        <div className='flex  justify-between mb-4'>
+            <div className='flex items-center'> {/* Wrap select and input in a flex container */}
+                <select
+                    value={searchName}
+                    onChange={(e) => setSearchName(e.target.value)}
+                    className='p-2 border border-gray-300 rounded mr-2' >
+                        
+                    <option value=''>All Employees</option>
+                    {employeeNames.map((name, index) => (
+                        <option key={index} value={name}>
+                            {name}
+                        </option>
+                    ))}
+                </select>
+                <input
+                    type='month'
+                    value={searchDate}
+                    onChange={(e) => setSearchDate(e.target.value)}
+                    className='p-2 border border-gray-300 rounded'/>
             </div>
         </div>
-    );
+
+
+
+            {loading? (<Spinner/>
+            ):(
+
+              <table className='w-full border-separate border-spacing-2'>
+    <thead>
+        <tr>
+            <th className='border border-slate-600 rounded-md'>No</th>
+            <th className='border border-slate-600 rounded-md'>EmpID</th>
+            <th className='border border-slate-600 rounded-md'>employeeName</th>
+            <th className='border border-slate-600 rounded-md max-md:hidden'>Date</th>
+            <th className='border border-slate-600 rounded-md max-md:hidden'>InTime</th>
+            <th className='border border-slate-600 rounded-md'>OutTime</th>
+            <th className='border border-slate-600 rounded-md'>Workinghours</th>
+            <th className='border border-slate-600 rounded-md'>OThours</th>
+            
+        </tr>
+    </thead>
+    <tbody>
+        {filteredEmployeesAttendence.map((EmployeeAttendence, index) => (
+
+            <tr key={EmployeeAttendence._id} className='h-8'>
+
+                <td className='border border-slate-700 rounded-md text-center'>
+                    {index + 1}
+                </td>
+                <td className='border border-slate-700 rounded-md text-center'>
+                    {EmployeeAttendence.EmpID}
+                </td>
+                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                    {EmployeeAttendence.employeeName}
+                </td>
+                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                    {EmployeeAttendence.Date}
+                </td>
+                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                    {EmployeeAttendence.InTime}
+                </td>
+                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                    {EmployeeAttendence.OutTime}
+                </td>
+
+                <td className='border border-slate-700 rounded-md text-center'>
+                    {EmployeeAttendence.WorkingHours}
+                </td>
+                
+                <td className='border border-slate-700 rounded-md text-center'>
+                    {EmployeeAttendence.OThours}
+                </td>
+                
+                
+              </tr>
+        ))}
+    </tbody>
+</table>
+            )}
+        
+      </div>
+    </div>
+  )
 }
 
-export default ShowEmployeeAttendence;
+export default ShowEmployeeAttendence
