91d9ec6e46309f4c4ac5294570be088b9f06364c
lasal dissanayake <122669223+LasalDissanayake@users.noreply.github.com>
2024-04-06 12:56:52 +0530

    report done

--- client/src/assets/pages/Inventory/ShowInventory.jsx
+++ client/src/assets/pages/Inventory/ShowInventory.jsx
@@ -1,16 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect, useRef } from 'react';
 import axios from "axios";
 import Spinner from "../../components/Spinner";
 import { Link } from "react-router-dom";
-import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
+import { AiOutlineEdit } from "react-icons/ai";
 import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import { useReactToPrint } from 'react-to-print';
 
 const ShowInventory = () => {
   const [inventory, setInventory] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [searchQuery, setSearchQuery] = useState("");
+  const componentRef = useRef();
 
   const handleSearch = async () => {
     setLoading(true);
@@ -44,55 +46,36 @@ const ShowInventory = () => {
       });
   }, []);
 
+  const generatePDF = useReactToPrint({
+    content: () => componentRef.current,
+    documentTitle: 'Inventory List',
+    onAfterPrint: () => alert('Data saved in PDF'),
+  });
+
   const applySearchFilter = (inventoryItem) => {
     if (!inventoryItem) return false;
-    return (
-      (String(inventoryItem.No) &&
-        String(inventoryItem.No)
-          .toLowerCase()
-          .includes(searchQuery.toLowerCase())) ||
-      (String(inventoryItem.Name) &&
-        String(inventoryItem.Name)
-          .toLowerCase()
-          .includes(searchQuery.toLowerCase())) ||
-      (String(inventoryItem.Location) &&
-        String(inventoryItem.Location)
-          .toLowerCase()
-          .includes(searchQuery.toLowerCase())) ||
-      (String(inventoryItem.Quantity) &&
-        String(inventoryItem.Quantity)
-          .toLowerCase()
-          .includes(searchQuery.toLowerCase())) ||
-      (String(inventoryItem.PurchasedPrice) &&
-        String(inventoryItem.PurchasedPrice)
-          .toLowerCase()
-          .includes(searchQuery.toLowerCase())) ||
-      (String(inventoryItem.SellPrice) &&
-        String(inventoryItem.SellPrice)
-          .toLowerCase()
-          .includes(searchQuery.toLowerCase())) ||
-      (String(inventoryItem.SupplierName) &&
-        String(inventoryItem.SupplierName)
-          .toLowerCase()
-          .includes(searchQuery.toLowerCase())) ||
-      (String(inventoryItem.SupplierPhone) &&
-        String(inventoryItem.SupplierPhone)
-          .toLowerCase()
-          .includes(searchQuery.toLowerCase())) ||
-      (String(inventoryItem.Operations) &&
-        String(inventoryItem.Operations)
-          .toLowerCase()
-          .includes(searchQuery.toLowerCase()))
+    const searchableFields = [
+      'No',
+      'Name',
+      'Location',
+      'Quantity',
+      'PurchasedPrice',
+      'SellPrice',
+      'SupplierName',
+      'SupplierPhone',
+      'Operations'
+    ];
+
+    return searchableFields.some(field =>
+      String(inventoryItem[field]).toLowerCase().includes(searchQuery.toLowerCase())
     );
   };
 
   const filteredInventory = inventory.filter(applySearchFilter);
 
   return (
-    <div className="p-4">
+    <div className="p-4" ref={componentRef}>
       <h1 className="text-3xl mb-8">Inventory List</h1>
-      
-      {/* Adjusting the position of the search bar by wrapping it in a div */}
       <div className="mb-4">
         <input
           type="text"
@@ -109,70 +92,45 @@ const ShowInventory = () => {
           Search
         </button>
       </div>
-      
-      {/* Display loading spinner or inventory table */}
       {loading ? (
         <Spinner />
       ) : (
-        <table className="w-full border-separate border-spacing-2">
+        <table className="w-full border-separate border-spacing-2"  ref={componentRef} >
+          
           <thead>
             <tr>
               <th className="border border-slate-600 rounded-md">No</th>
               <th className="border border-slate-600 rounded-md">Name</th>
               <th className="border border-slate-600 rounded-md">Location</th>
-              <th className="border border-slate-600 rounded-md max-md:hidden">
-                Quantity
-              </th>
-              <th className="border border-slate-600 rounded-md max-md:hidden">
-                Purchased Price
-              </th>
+              <th className="border border-slate-600 rounded-md max-md:hidden">Quantity</th>
+              <th className="border border-slate-600 rounded-md max-md:hidden">Purchased Price</th>
               <th className="border border-slate-600 rounded-md">Sell Price</th>
-              <th className="border border-slate-600 rounded-md">
-                Supplier Name
-              </th>
-              <th className="border border-slate-600 rounded-md">
-                Supplier Phone
-              </th>
+              <th className="border border-slate-600 rounded-md">Supplier Name</th>
+              <th className="border border-slate-600 rounded-md">Supplier Phone</th>
               <th className="border border-slate-600 rounded-md">Operations</th>
             </tr>
           </thead>
           <tbody>
             {filteredInventory.map((inventoryItem, index) => (
               <tr key={inventoryItem._id} className="h-8">
-                <td className="border border-slate-700 rounded-md text-center">
-                  {index + 1}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center">
-                  {inventoryItem.Name}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                  {inventoryItem.Location}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                  {inventoryItem.Quantity}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                  {inventoryItem.PurchasedPrice}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                  {inventoryItem.SellPrice}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                  {inventoryItem.SupplierName}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                  {inventoryItem.SupplierPhone}
-                </td>
+                <td className="border border-slate-700 rounded-md text-center">{index + 1}</td>
+                <td className="border border-slate-700 rounded-md text-center">{inventoryItem.Name}</td>
+                <td className="border border-slate-700 rounded-md text-center">{inventoryItem.Location}</td>
+                <td className="border border-slate-700 rounded-md text-center max-md:hidden">{inventoryItem.Quantity}</td>
+                <td className="border border-slate-700 rounded-md text-center max-md:hidden">{inventoryItem.PurchasedPrice}</td>
+                <td className="border border-slate-700 rounded-md text-center">{inventoryItem.SellPrice}</td>
+                <td className="border border-slate-700 rounded-md text-center">{inventoryItem.SupplierName}</td>
+                <td className="border border-slate-700 rounded-md text-center">{inventoryItem.SupplierPhone}</td>
                 <td className="border border-slate-700 rounded-md text-center">
                   <div className="flex justify-center gap-x-4">
                     <Link to={`/inventory/get/${inventoryItem._id}`}>
-                      <BsInfoCircle className="text-2x1 text-green-800" />
+                      <BsInfoCircle className="text-2xl text-green-800" />
                     </Link>
                     <Link to={`/inventory/edit/${inventoryItem._id}`}>
-                      <AiOutlineEdit className="text-2x1 text-yellow-600" />
+                      <AiOutlineEdit className="text-2xl text-yellow-600" />
                     </Link>
                     <Link to={`/inventory/delete/${inventoryItem._id}`}>
-                      <MdOutlineDelete className="text-2x1 text-red-600" />
+                      <MdOutlineDelete className="text-2xl text-red-600" />
                     </Link>
                   </div>
                 </td>
@@ -181,6 +139,11 @@ const ShowInventory = () => {
           </tbody>
         </table>
       )}
+      <div className="flex justify-center items-center mt-8">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={generatePDF}>
+          Generate PDF
+        </button>
+      </div>
     </div>
   );
 };
