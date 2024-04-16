d28b0c3940d9d2b27fa5735d06fcdb73b44ce84e
SithagiV <153867463+SithagiV@users.noreply.github.com>
2024-04-16 20:04:23 +0530

    add invoice button

--- client/src/assets/pages/Payment/ShowPayment.jsx
+++ client/src/assets/pages/Payment/ShowPayment.jsx
@@ -34,6 +34,10 @@ const ShowPayment=()=> {
                     Add Payment
                     </button>
                     <div style={{ marginLeft: '10px' }}></div> {/* Space between buttons */}
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => window.location.href='/PaymentInvoice/show'}>
+                    Invoices
+                    </button>
+                    <div style={{ marginLeft: '10px' }}></div> {/* Space between buttons */}
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => window.location.href='/payments/report'}>
                         Report
                     </button>
