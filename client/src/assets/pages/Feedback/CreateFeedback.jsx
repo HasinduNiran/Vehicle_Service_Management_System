c6984a9860ab8dcfa2baf74f4a3ef57071bf72f3
Yenura Sawan karunanayaka <Yenura02@gmail.com>
2024-04-19 20:36:40 +0530

    Update CreateFeedback.jsx

--- client/src/assets/pages/Feedback/CreateFeedback.jsx
+++ client/src/assets/pages/Feedback/CreateFeedback.jsx
@@ -1,26 +1,26 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import backgroundImage from "../../images/t.jpg";
 
 const CreateFeedback = () => {
-  // State variables for form inputs and loading state
-  const [cusID, setCustomerID] = useState("");
-  const [name, setName] = useState("");
+  const [cussID, setCustomerID] = useState("");
+  const [name, setCustomerName] = useState("");
   const [email, setEmail] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [employee, setEmployee] = useState("");
   const [starRating, setStarRating] = useState(1);
   const [dateOfService, setDateOfService] = useState(new Date());
+
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
-  const [employees, setEmployees] = useState([]); // State for storing employees data
-  const navigate = useNavigate(); // Hook for navigation
+  const [employees, setEmployees] = useState([]);
+  const navigate = useNavigate();
+  const { cusID } = useParams();
 
-  // Function to handle saving feedback
   const handleSaveFeedback = async () => {
-    // Check email and phone number format
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const phoneRegex = /^\d{10}$/;
     if (!emailRegex.test(email)) {
@@ -31,18 +31,13 @@ const CreateFeedback = () => {
       alert("Please enter a valid 10-digit phone number.");
       return;
     }
-
-    // Check if all fields are filled
-    if (!cusID || !name || !email || !phoneNumber || !employee || !message) {
+    if (!cussID || !name || !email || !phoneNumber || !employee || !message) {
       alert("Please fill in all fields before submitting.");
       return;
     }
-
-    // Format date_of_service
     const formattedDate = formatDate(dateOfService);
-
     const data = {
-      cusID: cusID,
+      cusID: cussID,
       name: name,
       email: email,
       phone_number: phoneNumber,
@@ -51,116 +46,180 @@ const CreateFeedback = () => {
       message: message,
       star_rating: starRating,
     };
-
     setLoading(true);
-
     try {
-      // Send POST request
       await axios.post("http://localhost:8076/feedback", data);
       setLoading(false);
-      navigate("/feedback"); // Navigate to feedback page after successful submission
+      navigate("/feedback");
     } catch (error) {
       setLoading(false);
       console.error("Error creating feedback:", error);
-      alert(
-        "An error occurred while creating feedback. Please try again later."
-      );
     }
   };
 
-  // Function to format date
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:8076/customer/${cusID}`)
+      .then((response) => {
+        const data = response.data;
+        setCustomerID(data.cusID);
+        setPhoneNumber(data.phone);
+        setEmail(data.email);
+        setCustomerName(`${data.firstName} ${data.lastName}`);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert(`An error happened. Please check console`);
+        console.log(error);
+      });
+  }, [cusID]);
+
   const formatDate = (date) => {
-    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
+    return `${year}-${month}-${day}`;
   };
 
-  // Fetch employees data on component mount
   useEffect(() => {
     const fetchEmployeesData = async () => {
       setLoading(true);
       try {
         const response = await axios.get("http://localhost:8076/employees");
-        const employeesData = response.data.data;
-        if (Array.isArray(employeesData)) {
-          setEmployees(employeesData);
+        // Assuming response.data is an array or can be transformed into one
+        if (Array.isArray(response.data)) {
+          setEmployees(response.data);
+        } else if (response.data && Array.isArray(response.data.employees)) {
+          // If the response has a different structure, extract the array
+          setEmployees(response.data.employees);
         } else {
-          console.error("Employees data is not an array:", employeesData);
+          console.error("Invalid response format for employees data:", response.data);
         }
-      } catch (error) {
-        console.error("Error fetching employees:", error);
-        alert("Failed to fetch employees. Please try again later.");
-      } finally {
         setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        console.error("Error fetching employees:", error);
       }
     };
-
     fetchEmployeesData();
-  }, [axios]); // Add axios dependency here
+  }, []);
+
+  const styles = {
+    container: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      minHeight: "100vh",
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+    },
+    formContainer: {
+      width: "50%",
+      backgroundColor: "rgba(5, 4, 2, 0.8)",
+      borderRadius: "10px",
+      boxShadow: "0 4px 6px rgba(0, 0, 0, 0.8)",
+      padding: "20px",
+      border: "2px solid red",
+      borderColor: "red",
+      margin: "10px",
+      textAlign: "center",
+      position: "relative",
+    },
+    label: {
+      fontWeight: "bold",
+      marginBottom: "0.5rem",
+      fontSize: "1.2rem",
+      color: "red",
+      textAlign: "center",
+      width: "100%",
+      display: "block",
+      textTransform: "uppercase",
+    },
+    input: {
+      padding: "8px",
+      borderRadius: "5px",
+      border: "1px solid #ccc",
+      width: "100%",
+      color: "white",
+      backgroundColor: "black",
+      marginBottom: "10px",
+    },
+    button: {
+      backgroundColor: "red",
+      color: "#fff",
+      border: "none",
+      borderRadius: "0.25rem",
+      fontWeight: "bold",
+      padding: "0.5rem 1rem",
+      cursor: "pointer",
+      transition: "background-color 0.3s ease",
+    },
+  };
 
   return (
-    <div className="p-4">
-      <h1 className="text-3xl my-4">Create Feedback</h1>
-
-      {loading && <p>Loading...</p>}
-
-      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
-        {/* Form inputs */}
-        <div className="p-4">
-          <label className="text-xl mr-4 text-gray-500">Customer ID</label>
+    <div style={styles.container}>
+      <div style={styles.formContainer}>
+        <h1>Create Feedback</h1>
+        <div>
+          <label style={styles.label}>Customer ID</label>
           <input
             type="text"
-            value={cusID}
+            value={cussID}
             onChange={(e) => setCustomerID(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
+            style={styles.input}
           />
         </div>
-        <div className="p-4">
-          <label className="text-xl mr-4 text-gray-500">Name</label>
+        <div>
+          <label style={styles.label}>Name</label>
           <input
             type="text"
             value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
+            onChange={(e) => setCustomerName(e.target.value)}
+            style={styles.input}
           />
         </div>
-        <div className="p-4">
-          <label className="text-xl mr-4 text-gray-500">Email</label>
+        <div>
+          <label style={styles.label}>Email</label>
           <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
+            style={styles.input}
           />
         </div>
-        <div className="p-4">
-          <label className="text-xl mr-4 text-gray-500">Phone Number</label>
+        <div>
+          <label style={styles.label}>Phone Number</label>
           <input
             type="tel"
             value={phoneNumber}
             onChange={(e) => setPhoneNumber(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
+            style={styles.input}
           />
         </div>
-        <div className="p-4">
-          <label className="text-xl mr-4 text-gray-500">Employee</label>
+        <div>
+          <label style={styles.label}>Employee</label>
           <select
             value={employee}
             onChange={(e) => setEmployee(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
+            style={styles.input}
           >
             <option value="">Select Employee</option>
             {employees.map((employee) => (
               <option key={employee.id} value={employee.id}>
-                {employee.employeeName}
+                {employee.name}
               </option>
             ))}
           </select>
         </div>
-        <div className="p-4">
-          <label className="text-xl mr-4 text-gray-500">Star Rating</label>
+        <div>
+          <label style={styles.label}>Star Rating</label>
           <select
             value={starRating}
             onChange={(e) => setStarRating(parseInt(e.target.value))}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
+            style={styles.input}
           >
             {[1, 2, 3, 4, 5].map((rating) => (
               <option key={rating} value={rating}>
@@ -169,25 +228,24 @@ const CreateFeedback = () => {
             ))}
           </select>
         </div>
-        <div className="p-4">
-          <label className="text-xl mr-4 text-gray-500">Date of Service</label>
+        <div>
+          <label style={styles.label}>Date of Service</label>
           <DatePicker
             selected={dateOfService}
             onChange={(date) => setDateOfService(date)}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
+            dateFormat="yyyy-MM-dd"
+            className="date-picker-input"
           />
         </div>
-        <div className="p-4">
-          <label className="text-xl mr-4 text-gray-500">Message</label>
+        <div>
+          <label style={styles.label}>Message</label>
           <textarea
             value={message}
             onChange={(e) => setMessage(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
+            style={styles.input}
           />
         </div>
-        
-        {/* Button to submit feedback */}
-        <button className="p-2 bg-sky-300 m-8" onClick={handleSaveFeedback}>
+        <button style={styles.button} onClick={handleSaveFeedback}>
           {loading ? "Creating..." : "Create"}
         </button>
       </div>
