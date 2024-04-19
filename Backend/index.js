9730f0910751df534755bc242103fb03492e2a3a
Niran B G S H <127426411+HasinduNiran@users.noreply.github.com>
2024-04-19 14:47:31 +0530

    update image not working

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,4 @@
+
 // Importing necessary modules
 import express from "express";
 import mongoose from "mongoose";
@@ -6,10 +7,7 @@ import cors from 'cors';
 // Importing custom configurations
 import { PORT, mongoDBURL } from './config.js';
 
-
-// Importing model and routes
-
-
+// Importing routes
 import UserAccount_Route from './Routes/UserAccount_Route.js';
 import Employee_Route from './Routes/Employee_Route.js';
 import Inventory_Route from './Routes/Inventory_Route.js';
@@ -29,64 +27,31 @@ import EmployeeSalary_Route from './Routes/EmployeeSalary_Route.js';
 // Creating an instance of the Express application
 const app = express();
 
-
 // Middleware for parsing request body
 app.use(express.json());
 
 // Middleware for handling CORS POLICY
-// Option 1: Allow All Origins with Default of cors(*)
 app.use(cors());
-// Option 2: Allow Custom Origins
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
-//   })
-// );
 
-// app.get('/', (request, response) => {
-//   console.log(request);
-//   return response.status(234).send('Welcome To MERN Stack Tutorial');
-// });
-
-
-// Using  routes for endpoint
-
-
-
-
-
-// app.get('/',(request,response)=>{
-//    console.log(request);
-//    return response.status(234).send('Welcome')
-// });
-
-
-
+// Using routes for endpoints
 app.use('/customer', UserAccount_Route);
-app.use('/employees',Employee_Route);
+app.use('/employees', Employee_Route);
 app.use('/vehicles', Vehicle_Route);
 app.use('/inventory', Inventory_Route);
-app.use('/payments',Payment_Route);
+app.use('/payments', Payment_Route);
 app.use('/feedback', Feedback_Route);
-app.use('/bookings',Booking_Route);
-app.use('/Package',Package_Route);
-app.use('/Service',Service_Route);
-app.use('/ServiceHistory',ServiceHistory_Route);
-app.use('/Manager',Manager_Route);
-app.use('/EmployeeAttendence',EmployeeAttendence_Route);
-
-app.use('/PaymentInvoice',PaymentInvoice_Route);
-
-app.use('/Home',ReadOneHome_Route);
-app.use('/EmployeeSalary',EmployeeSalary_Route);
-
+app.use('/bookings', Booking_Route);
+app.use('/Package', Package_Route);
+app.use('/Service', Service_Route);
+app.use('/ServiceHistory', ServiceHistory_Route);
+app.use('/Manager', Manager_Route);
+app.use('/EmployeeAttendence', EmployeeAttendence_Route);
+app.use('/PaymentInvoice', PaymentInvoice_Route);
+app.use('/Home', ReadOneHome_Route);
+app.use('/EmployeeSalary', EmployeeSalary_Route);
 
 // Connecting to the MongoDB database
-
-mongoose
-  .connect(mongoDBURL)
+mongoose.connect(mongoDBURL)
   .then(() => {
     console.log('App connected to database');
     app.listen(PORT, () => {
@@ -96,5 +61,3 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
-
-  
