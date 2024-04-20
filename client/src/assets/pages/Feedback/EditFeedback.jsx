99668485b08e258d7dd99b06aa3b27bde28517f2
Niran B G S H <127426411+HasinduNiran@users.noreply.github.com>
2024-04-20 22:37:54 +0530

    update edit feedback

--- client/src/assets/pages/Feedback/EditFeedback.jsx
+++ client/src/assets/pages/Feedback/EditFeedback.jsx
@@ -1,29 +1,31 @@
 import React, { useState, useEffect } from "react";
-import Spinner from "../../components/Spinner";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import backgroundImage from "../../images/t.jpg";
+import { FaBolt, FaStar } from "react-icons/fa";
 
 const EditFeedback = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [employee, setEmployee] = useState("");
   const [dateOfService, setDateOfService] = useState(new Date());
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
-  const [emailError, setEmailError] = useState("");
-  const [phoneNumberError, setPhoneNumberError] = useState("");
-  const [employees, setEmployees] = useState([]);
   const [starRating, setStarRating] = useState(null);
+  const [employees, setEmployees] = useState([]);
   const [feedbackId, setFeedbackId] = useState("");
   const { id } = useParams();
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetchFeedbackData();
-    fetchEmployeesData();
+    if (id) {
+      fetchFeedbackData();
+      fetchEmployeesData();
+    }
   }, [id]);
 
   const fetchFeedbackData = () => {
@@ -74,8 +76,13 @@ const EditFeedback = () => {
   };
 
   const handleSaveFeedback = () => {
+    const isValidEmail = (email) => {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+    };
+
     setEmailError("");
-    setPhoneNumberError("");
+    setPhoneNumber("");
 
     if (
       !name ||
@@ -86,6 +93,18 @@ const EditFeedback = () => {
       !message
     ) {
       alert("Please fill in all fields before submitting.");
+      return;
+    }
+
+    if (!isValidEmail(email)) {
+      setEmailError("Please enter a valid email address.");
+      return;
+    }
+
+    const phoneRegex = /^\d{10}$/;
+
+    if (!phoneRegex.test(phoneNumber)) {
+      alert("Please enter a valid phone number.");
       return;
     }
 
@@ -115,53 +134,78 @@ const EditFeedback = () => {
         setLoading(false);
       });
   };
+  const Spinner = () => {
+    return <div className="spinner"></div>;
+  };
+
+  const renderStars = () => {
+    return (
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        {[...Array(5)].map((_, index) => (
+          <FaStar
+            key={index}
+            className={index < starRating ? "star-filled" : "star-empty"}
+            onMouseOver={() => handleStarHover(index)}
+            onClick={() => handleStarClick(index)}
+            style={{
+              color: index < starRating ? "red" : "gray",
+              height: "50px",width:"50px",
+            }}
+          />
+        ))}
+      </div>
+    );
+  };
+
+  const handleStarClick = (index) => {
+    setStarRating(index + 1);
+  };
+
+  const handleStarHover = (index) => {
+    setStarRating(index + 1);
+  };
 
   return (
-    <div className="p-4">
-      <a href="/feedback" className="text-blue-500 hover:underline mb-2 block">
-        Back to Feedback
-      </a>
-      <h1 className="text-3xl my-4">Edit Feedback</h1>
-      {loading && <Spinner />}
-      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+    <div style={styles.container}>
+      <div style={styles.formContainer}>
+        <a href="/feedback" style={styles.button}>
+          Back to Feedback
+        </a>
+        <div>
+        <h1 style={styles.heading}>Edit Feedback</h1>
+        {loading && <Spinner />}
+        </div>
         <div className="p-4">
-          <label className="text-xl mr-4 text-gray-500">Name</label>
+          <label style={styles.label}>Name</label>
           <input
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
+            style={styles.input}
           />
-          <label className="text-xl mr-4 text-gray-500">Email</label>
+          <label style={styles.label}>Email</label>
           <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className={`border-2 border-gray-500 px-4 py-2 w-full ${
-              emailError && "border-red-500"
-            }`}
+            style={styles.input}
+            className={` ${emailError && "border-red-500"}`}
           />
           {emailError && <p className="text-red-500 text-sm">{emailError}</p>}
-
-          <label className="text-xl mr-4 text-gray-500">Phone Number</label>
+          <label style={styles.label}>Phone Number</label>
           <input
             type="tel"
             value={phoneNumber}
             onChange={(e) => setPhoneNumber(e.target.value)}
-            className={`border-2 border-gray-500 px-4 py-2 w-full ${
-              phoneNumberError && "border-red-500"
-            }`}
+            style={styles.input}
           />
-          {phoneNumberError && (
-            <p className="text-red-500 text-sm">{phoneNumberError}</p>
-          )}
-
           <div className="p-4">
-            <label className="text-xl mr-4 text-gray-500">Employee</label>
+            <label style={styles.label}>Employee</label>
             <select
               value={employee}
               onChange={(e) => setEmployee(e.target.value)}
-              className="border-2 border-gray-500 px-4 py-2 w-full"
+              className="border-2 border-gray-500 px-4 py-2 w-full custom-select"
+              style={styles.input}
             >
               <option value="">Select Employee</option>
               {employees.map((employee) => (
@@ -171,44 +215,113 @@ const EditFeedback = () => {
               ))}
             </select>
           </div>
-
-          <label className="text-xl mr-4 text-gray-500">Date Of Service</label>
+          <div>
+            <label style={styles.label}>Star Rating</label>
+            {renderStars()}
+          </div>
+          <label style={styles.label}>Date Of Service</label>
           <DatePicker
             selected={dateOfService}
             onChange={(date) => setDateOfService(date)}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
           />
-
-          <label className="text-xl mr-4 text-gray-500">Message</label>
+          <label style={styles.label}>Message</label>
           <input
             type="text"
             value={message}
+            style={styles.text}
             onChange={(e) => setMessage(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
           />
-
-          <label className="text-xl mr-4 text-gray-500">Star Rating</label>
-          <select
-            value={starRating || ""}
-            onChange={(e) => setStarRating(parseInt(e.target.value))}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
-          >
-            <option value="">Select Star Rating</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-
-          <div className="flex justify-center"></div>
         </div>
-        <button className="p-2 bg-sky-300 m-8" onClick={handleSaveFeedback}>
-          Save
-        </button>
+        <div style={styles.buttonContainer}>
+          <button
+            style={styles.button}
+            disabled={loading}
+            onClick={handleSaveFeedback}
+          >
+            {loading ? "Editing..." : "Edit Feedback"}
+            Save
+          </button>
+        </div>
       </div>
     </div>
   );
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "100vh",
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  formContainer: {
+    width: "80%",
+    backgroundColor: "rgba(5, 4, 2, 0.8)",
+    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.8)",
+    padding: "20px",
+    border: "2px solid red",
+    borderColor: "red",
+    margin: "10px",
+    textAlign: "center",
+    position: "relative",
+    alignItems: "center",
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: "0.5rem",
+    fontSize: "1.2rem",
+    color: "red",
+    textAlign: "center",
+    width: "100%",
+    padding: "10px",
+    textTransform: "uppercase",
+  },
+  input: {
+    width: "60%",
+    padding: "10px",
+    borderRadius: "5px",
+    border: "1px solid #ccc",
+    backgroundColor: "black",
+    color: "white",
+    fontSize: "1.2rem",
+    marginBottom: "10px",
+    textAlign: "left",
+  },
+  text: {
+    width: "65%",
+    height: "100px",
+    padding: "10px",
+    borderRadius: "5px",
+    border: "1px solid #ccc",
+    backgroundColor: "black",
+    color: "white",
+    fontSize: "1.2rem",
+    marginBottom: "10px",
+    textAlign: "left",
+  },
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "#ff0000",
+    color: "black",
+    FaBolt: "true",
+    padding: "10px 20px",
+    borderRadius: "5px",
+    border: "none",
+    cursor: "pointer",
+    transition: "background-color 0.8s",
+  },
+  heading: {
+    fontSize: '3rem',
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold'
+  },
 };
 
 export default EditFeedback;
