6408e11d9fbf3fa5d822b8f122560ca3ee62a594
Niran B G S H <127426411+HasinduNiran@users.noreply.github.com>
2024-04-21 01:08:34 +0530

    Update PackageDashboard.jsx

--- client/src/assets/pages/SPackage/PackageDashboard.jsx
+++ client/src/assets/pages/SPackage/PackageDashboard.jsx
@@ -49,7 +49,7 @@ function PackageDashboard() {
     const handleDeletePackage = (id) => {
         Swal.fire({
             title: "Are you sure?",
-            text: "You won't be able to revert this!",
+            text: "You won't be able to revert this !!",
             icon: "warning",
             showCancelButton: true,
             confirmButtonColor: "#3085d6",
