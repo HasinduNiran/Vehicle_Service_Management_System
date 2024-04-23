3375667e3844871dc67b3ef6bcc448ee1396da19
Yenura Sawan karunanayaka <Yenura02@gmail.com>
2024-04-23 22:51:27 +0530

    Update Feedback_Route.js

--- Backend/Routes/Feedback_Route.js
+++ Backend/Routes/Feedback_Route.js
@@ -98,32 +98,27 @@ router.get("/employees/names", async (req, res) => {
   }
 });
 
-// GET route for retrieving filtered feedback based on search criteria
+// GET route for retrieving feedback based on search criteria
 router.get("/feedback", async (req, res) => {
   try {
-    const { search = "", sort = "name" } = req.query;
-
-    // Define search query
-    const query = {
-      $or: [
-        { cusID: { $regex: search, $options: "i" } },
-        { name: { $regex: search, $options: "i" } },
-        { email: { $regex: search, $options: "i" } },
-        { phone_number: { $regex: search, $options: "i" } },
-        { employee: { $regex: search, $options: "i" } },
-        { date_of_service: { $regex: search, $options: "i" } },
-        { star_rating: { $regex: search, $options: "i" } },
-      ],
-    };
-
-    // Fetch filtered feedback based on search criteria and sort
-    const feedback = await Feedback.find(query)
-      .sort({ [sort]: 1 });
-
-    res.status(200).json({ count: feedback.length, data: feedback });
+      const { search = "" } = req.query;
+      const query = {
+          $or: [
+              { cusID: { $regex: search, $options: "i" } },
+              { name: { $regex: search, $options: "i" } },
+              { email: { $regex: search, $options: "i" } },
+              { phone_number: { $regex: search, $options: "i" } },
+              { employee: { $regex: search, $options: "i" } },
+              { date_of_service: { $regex: search, $options: "i" } },
+              { message: { $regex: search, $options: "i" } },
+              { star_rating: { $regex: search, $options: "i" } },
+          ],
+      };
+      const feedback = await Feedback.find(query);
+      res.status(200).json({ count: feedback.length, data: feedback });
   } catch (err) {
-    console.error(err.message);
-    res.status(500).json({ error: true, message: "Internal Server Error" });
+      console.error(err.message);
+      res.status(500).json({ error: true, message: "Internal Server Error" });
   }
 });
 
