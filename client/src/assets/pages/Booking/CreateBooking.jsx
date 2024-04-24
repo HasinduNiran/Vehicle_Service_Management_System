90a5f99b00a4e2ac94850226d3c2e849ea219e69
Niran B G S H <127426411+HasinduNiran@users.noreply.github.com>
2024-04-24 13:52:25 +0530

    path change

--- client/src/assets/pages/Booking/CreateBooking.jsx
+++ client/src/assets/pages/Booking/CreateBooking.jsx
@@ -117,7 +117,7 @@ const CreateBooking = () => {
             .then(() => {
                 setLoading(false);
                 alert('Your Booking is successfull');
-                navigate('/customer/get/${cusID}');
+                navigate(`/ReadOneHome/${cusID}`);
             })
             .catch((error) => {
                 setLoading(false);
