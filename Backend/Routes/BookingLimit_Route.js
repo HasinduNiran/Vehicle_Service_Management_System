341e3a84ac3ee48796622b29a262fe50112e0691
Isuru66 <145270583+Isuru66@users.noreply.github.com>
2024-05-12 15:57:25 +0530

    Update BookingLimit_Route.js
    booking limit add kra

--- Backend/Routes/BookingLimit_Route.js
+++ Backend/Routes/BookingLimit_Route.js
@@ -65,7 +65,7 @@ router.post('/',async (request, response) => {
 
     // });
 
-
+//
 // GETS route to retrieve a booking limit by ID or Booking_Date
      router.get('/:identifier', async (req, res) => {
     try {
